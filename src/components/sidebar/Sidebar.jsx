a086cc9510400988c3cf60aeca4b9fdacbb4fa24
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-07-30 20:59:55 +0300

    new

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,49 @@
 import './Sidebar.css'
-
+import { RssFeed ,Chat,Group,PlayCircle,QuestionAnswer,Work,EmojiEvents,School} from '@mui/icons-material'
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 function Sidebar() {
   return (
     <div className='sidebar'>
-      sidebar
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircle className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <BookmarkIcon className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <QuestionAnswer className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Work className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <EmojiEvents className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className='sidebarIcon'/>
+            <span className="spansidebarListItemText">Feed</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
