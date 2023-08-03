7f733cec33e94077c0b7b183ba0756ee6b2c53c8
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-08-03 06:25:36 +0300

    feed done

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,16 @@
 import './Feed.css'
 import Share from "../share/Share"
+import Post from "../post/Post"
 function Feed() {
   return (
     <div className='feed'>
       <div className="feedWrapper">
         <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   )
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,5 @@
 import "./Share.css";
-import {PermMedia} from "@mui/icons-material"
+import {PermMedia, Label, Room , EmojiEmotions} from "@mui/icons-material"
 function Share() {
   return (
     <div className="share">
@@ -12,10 +12,23 @@ function Share() {
             <div className="shareBottom">
                 <div className="shareOptions">
                     <div className="shareOption">
-                        <PermMedia className="shareIcon"/>
+                        <PermMedia htmlColor="tomato" className="shareIcon"/>
                         <span className="shareOptionText">Photo or Video</span>
                     </div>
+                    <div className="shareOption">
+                        <Label htmlColor="blue" className="shareIcon"/>
+                        <span className="shareOptionText">Tag</span>
+                    </div>
+                    <div className="shareOption">
+                        <Room htmlColor="green" className="shareIcon"/>
+                        <span className="shareOptionText">Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                        <span className="shareOptionText">Feeling</span>
+                    </div>
                 </div>
+                <button className="shareButton">Share</button>
             </div>
 
         </div>
