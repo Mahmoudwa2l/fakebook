5cf26d68acff91a9ce68b942f30a4d9048a17b22
Mahmoudwa2l <mahmoud.wael5@msa.edu.eg>
2023-08-06 20:57:59 +0300

    new rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,34 @@
-import './Rightbar.css'
+import "./Rightbar.css";
 
 function Rightbar() {
   return (
-    <div className='rightbar'>
-      rightbar
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Mahmoud Wael</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+              
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Rightbar
+export default Rightbar;
